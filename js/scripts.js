708683119f21b36dece708020aa558878ccef64d
Bryan <bryan.borgeson@gmail.com>
2015-04-14 16:10:05 -0700

    Write method for fullAddress

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,30 @@ $(document).ready(function() {
         // Attributes - get value from input fields in the form and store them in variables
         var inputtedFirstName = $("input#new-first-name").val();
         var inputtedLastName  = $("input#new-last-name").val();
-        var inputtedAddress   = $("input#new-address").val();
+        var inputtedStreet    = $("input#new-street").val();
+        var inputtedCity      = $("input#new-city").val();
+        var inputtedState     = $("input#new-state").val();
+        var inputtedZip       = $("input#new-zip").val();
         var inputtedPhone     = $("input#new-phone").val();
         var inputtedEmail     = $("input#new-email").val();
 
+        var newAddress = {
+                street:      inputtedStreet,
+                city:        inputtedCity,
+                state:       inputtedState,
+                zip:         inputtedZip,
+                fullAddress: function() {
+                    return this.street + " " + this.city + ", " + this.state + " " + this.zip;
+                }
+            };
+
         // Define a 'Contact' Object
         var newContact = {
-                    firstName: inputtedFirstName,
-                    lastName:  inputtedLastName,
-                    address:   inputtedAddress,
-                    phone:     inputtedPhone,
-                    email:     inputtedEmail
+                    firstName:   inputtedFirstName,
+                    lastName:    inputtedLastName,
+                    address:     newAddress.fullAddress(),
+                    phone:       inputtedPhone,
+                    email:       inputtedEmail
                 };
 
         // Populates the list of clickable names
@@ -25,7 +38,10 @@ $(document).ready(function() {
         // Resets the inputs to be blank
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
-        $("input#new-address").val("");
+        $("input#new-street").val("");
+        $("input#new-city").val("");
+        $("input#new-state").val("");
+        $("input#new-zip").val("");
         $("input#new-phone").val("");
         $("input#new-email").val("");
 
@@ -33,8 +49,6 @@ $(document).ready(function() {
         $(".contact").last().click(function() {
             $("#show-contact").show();
             $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-            $(".first-name").text(newContact.firstName);
-            $(".last-name").text(newContact.lastName);
             $(".address").text(newContact.address);
             $(".phone").text(newContact.phone);
             $(".email").text(newContact.email);
